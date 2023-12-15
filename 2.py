8e8285f54280ed88cba2555a2bedebe0d11f7dd5
callme-X <giri090205@gmail.com>
2023-12-15 21:48:28 +0530

    crlf error fix

---
+++ 2.py
@@ -0,0 +1,31 @@
+#All Arithematic and logic operation
+if True == False:
+    A = int(input("enter number for A = "))
+    B = int(input("enter mumber for B = "))
+    print(": Addition is the Arithematic function :", A + B)
+    print("This is relational operator < :", A < B, "\n\n" )
+    
+    A = int(input("enter number for A = "))
+    B = int(input("enter mumber for B = "))
+    print(" Subtration is the Arithematic function :", A - B)
+    print("This is relational operator < :", A > B, "\n\n" )
+    
+    A = float(input("enter number for A = "))
+    B = float(input("enter mumber for B = "))
+    print("mutiplication is the Arithematic function :", A * B)
+    print("This is relational operator < :", A >= B, "\n\n" )
+    
+    A = float(input("enter number for A = "))
+    B = float(input("enter mumber for B = "))
+    print(": Division is the Arithematic function :", A / B)
+    print("This is relational operator < :", A <= B,"\n\n" )
+    
+    A = float(input("enter number for A = "))
+    B = float(input("enter mumber for B = "))
+    print(": Modulus is the Arithematic function :", A % B)
+    print("This is relational operator < :", A == B,"\n\n")
+
+    A = float(input("enter number for A = "))
+    B = float(input("enter mumber for B = "))
+    print(": exeponent is the Arithematic function :", A ** B)
+    print("This is relational operator < :", A != B,"\n\n" )
